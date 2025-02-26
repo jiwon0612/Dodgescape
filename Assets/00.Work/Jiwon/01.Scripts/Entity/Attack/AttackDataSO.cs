d1b42a73c2b40432b9b3db88e9605ef895860400
koori0831 <fuyunokoori0831@gmail.com>
2025-02-27 02:22:09 +0900

    Add: Weapon

--- Assets/00.Work/Jiwon/01.Scripts/Entity/Attack/AttackDataSO.cs
+++ Assets/00.Work/Jiwon/01.Scripts/Entity/Attack/AttackDataSO.cs
@@ -6,4 +6,5 @@ public class AttackDataSO : ScriptableObject
     public float damage;
     public float movement;
     public float attackPower;
+    public float stunDuration;
 }
--- Assets/00.Work/koori/01.Scripts/Weapon/Weapon.cs
+++ Assets/00.Work/koori/01.Scripts/Weapon/Weapon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private AttackDataSO _attackData;
+    [SerializeField] private EntityFinderSO _entityFinder;
 
     private List<int> hitEntities = new List<int>();
 
@@ -16,12 +17,12 @@ public class Weapon : MonoBehaviour
             Vector3 dir = (entity.transform.position - transform.position).normalized;
             Vector2 knockBack = new Vector2(dir.x, dir.z) * _attackData.attackPower;
 
-            //entity.TakeDamage(_attackData.damage);
+            entity.TakeDamage(_attackData.damage, knockBack, _attackData.stunDuration, _entityFinder.target);
             hitEntities.Add(entity.GetInstanceID());
         }
     }
 
-    private void OnEnable()
+    private void OnEndAttack()
     {
         hitEntities.Clear();
     }
