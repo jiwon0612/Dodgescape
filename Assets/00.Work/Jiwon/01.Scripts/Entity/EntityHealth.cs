fa0894c143e5e959578862101a50cda5e34376b9
jiwon0612 <iololloli9976@gmail.com>
2025-03-04 11:26:09 +0900

    fix : kooriMap

--- Assets/00.Work/Jiwon/01.Scripts/Entity/EntityHealth.cs
+++ Assets/00.Work/Jiwon/01.Scripts/Entity/EntityHealth.cs
@@ -35,6 +35,7 @@ public class EntityHealth : MonoBehaviour ,IEntityComp
 
         if (IsEvasion)
         {
+            Debug.Log("dd");
             OnEvasionEvent?.Invoke(dealer);
             return;
         }
